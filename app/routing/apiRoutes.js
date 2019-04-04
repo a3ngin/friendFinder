38de9d44f8402a374216811bb913522d680a5d80
Anne Engin <anne@Annes-MacBook-Air.local>
2019-04-04 17:38:31 -0400

    a lot done

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var friends = require('../data/friends.js');
+
+module.exports = function(app) {
+
+	app.get('/api/friends', function(req, res) {
+		res.json(friends);
+    });
+
+	app.post('/api/friends', function(req, res) {
+    let addFriend = req.body;
+    let difference = 90;
+
+    for (i=0; i <friends.length; i++){
+        let newTotal = 0;
+        for (j=0; j   <friends[i].scores.length; j++)  {
+            newTotal += Math.abs(newFriend.scores[j] - friends[i].scores[j]);
+        }
+
+        if (newTotal < difference) {
+            match = friends[i];
+            difference = newTotal;
+        }
+    }
+
+    })
+    
+}
