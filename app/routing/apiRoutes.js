3f657cc92445d399ea765b9f8d4f9155db24b20f
Anne Engin <anne@Annes-MacBook-Air.local>
2019-04-04 18:34:51 -0400

    heroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,28 +1,30 @@
 var path = require('path');
 var friends = require('../data/friends.js');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-	app.get('/api/friends', function(req, res) {
-		res.json(friends);
+    app.get('/api/friends', function (req, res) {
+        res.json(friends);
     });
 
-	app.post('/api/friends', function(req, res) {
-    let addFriend = req.body;
-    let difference = 90;
+    app.post('/api/friends', function (req, res) {
+        let addFriend = req.body;
+        let difference = 900;
 
-    for (i=0; i <friends.length; i++){
-        let newTotal = 0;
-        for (j=0; j   <friends[i].scores.length; j++)  {
-            newTotal += Math.abs(newFriend.scores[j] - friends[i].scores[j]);
-        }
+        for (i = 0; i < friends.length; i++) {
+            let newTotal = 0;
+            for (j = 0; j < friends[i].scores.length; j++) {
+                newTotal += Math.abs(parseInt(addFriend.scores[j]) - parseInt(friends[i].scores[j]))
+            }
 
-        if (newTotal < difference) {
-            match = friends[i];
-            difference = newTotal;
+            if (newTotal < difference) {
+                match = friends[i];
+                difference = newTotal;
+            }
+            friends.push(addFriend)
+            res.json(match)
         }
-    }
 
     })
-    
+
 }
